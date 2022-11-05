b158f5a91daacf1a5c178880ec9fc2d544d91a84
zy <1661996964@qq.com>
2022-11-05 17:35:50 +0800

    从IoC容器中获取bean的两种方式

--- src/main/java/com/zy/spring/ioc/SpringApplication.java
+++ src/main/java/com/zy/spring/ioc/SpringApplication.java
@@ -1,11 +1,18 @@
 package com.zy.spring.ioc;
 
+import com.zy.spring.ioc.entity.Apple;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringApplication {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        //从IoC容器中获取bean
+        Apple sweetApple = context.getBean("apple4", Apple.class);
+        System.out.println(sweetApple.getTitle());
+        //从IoC容器中获取bean
+        Apple apple2 =(Apple)context.getBean("apple2");
+        System.out.println(apple2.getTitle());
 
     }
 }
