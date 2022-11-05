8192bf85a62b206fbca3767a7121c0163e4042ba
zy <1661996964@qq.com>
2022-11-05 19:17:00 +0800

    路径表达式：加载单个文件和加载多个文件

--- src/main/java/com/zy/spring/ioc/SpringApplication.java
+++ src/main/java/com/zy/spring/ioc/SpringApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringApplication {
     public static void main(String[] args) {
+        //路径表达式：这是加载单个文件ClassPathXmlApplicationContext
        // ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
         //从IoC容器中获取bean
 //        Apple sweetApple = context.getBean("apple4", Apple.class);
@@ -14,6 +15,7 @@ public class SpringApplication {
 //        Apple apple2 =(Apple)context.getBean("apple2");
 //        System.out.println(apple2.getTitle());
 
+        //路径表达式：这是加载多个文件ClassPathXmlApplicationContext
         //将配置文件的顺序调整，后面的apple2的bean会覆盖前面的bean
         String[] configLocations = new String[]{"classpath:applicationContext.xml","classpath:applicationContext-1.xml"};
         ApplicationContext context = new ClassPathXmlApplicationContext(configLocations);
