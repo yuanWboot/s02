c547fe6afb632c369427e2b03a8d4605fc5db231
zy <1661996964@qq.com>
2022-11-05 19:02:55 +0800

    将配置文件的顺序调整，后面的apple2的bean会覆盖前面的bean

--- src/main/java/com/zy/spring/ioc/SpringApplication.java
+++ src/main/java/com/zy/spring/ioc/SpringApplication.java
@@ -6,12 +6,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringApplication {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+       // ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
         //从IoC容器中获取bean
-        Apple sweetApple = context.getBean("apple4", Apple.class);
-        System.out.println(sweetApple.getTitle());
-        //从IoC容器中获取bean
-        Apple apple2 =(Apple)context.getBean("apple2");
+//        Apple sweetApple = context.getBean("apple4", Apple.class);
+//        System.out.println(sweetApple.getTitle());
+//        //从IoC容器中获取bean
+//        Apple apple2 =(Apple)context.getBean("apple2");
+//        System.out.println(apple2.getTitle());
+
+        //将配置文件的顺序调整，后面的apple2的bean会覆盖前面的bean
+        String[] configLocations = new String[]{"classpath:applicationContext.xml","classpath:applicationContext-1.xml"};
+        ApplicationContext context = new ClassPathXmlApplicationContext(configLocations);
+        Apple apple2 = context.getBean("apple2", Apple.class);
         System.out.println(apple2.getTitle());
 
     }
