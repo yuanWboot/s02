fa3b9a4680a8e6636bb838010af97092c3cbad52
zy <1661996964@qq.com>
2022-11-05 16:24:04 +0800

    通过构造方法创建对象

---
+++ src/main/java/com/zy/spring/ioc/entity/Apple.java
@@ -0,0 +1,42 @@
+package com.zy.spring.ioc.entity;
+
+public class Apple {
+    private String title;
+    private String color;
+    private String origin;
+
+    public Apple() {
+        System.out.println("Apple对象已创建"+this);
+    }
+
+    public Apple(String title, String color, String origin) {
+        System.out.println("通过构造方法创建对象，"+this);
+        this.title = title;
+        this.color = color;
+        this.origin = origin;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+}
---
+++ src/main/java/com/zy/spring/ioc/entity/Child.java
@@ -0,0 +1,30 @@
+package com.zy.spring.ioc.entity;
+
+public class Child {
+    private String name;
+    private Apple apple;
+
+    public Child() {
+    }
+
+    public Child(String name, Apple apple) {
+        this.name = name;
+        this.apple = apple;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Apple getApple() {
+        return apple;
+    }
+
+    public void setApple(Apple apple) {
+        this.apple = apple;
+    }
+}
